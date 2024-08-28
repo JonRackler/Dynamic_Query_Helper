7eb5a609e6df08f797369707903419b9f2a0d75b
JonRackler <jonrackler@gmail.com>
2024-08-28 07:32:00 -0500

    Added quit button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,18 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+<<<<<<< HEAD
+=======
+
+    QObject::connect(ui->quitButton, SIGNAL (clicked()), QApplication::instance(),SLOT (quit()));
+>>>>>>> 9c0b0f6 (Added quit button)
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+<<<<<<< HEAD
 
+=======
+>>>>>>> 9c0b0f6 (Added quit button)
