5c3f1ee36ebc1ed00b87625c149f9b4cd4c302f7
JonRackler <jonrackler@gmail.com>
2024-08-30 10:37:44 -0500

    Remove conflict markers from files

--- mainwindow.h
+++ mainwindow.h
@@ -4,13 +4,10 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-<<<<<<< HEAD
 namespace Ui { class MainWindow; }
-=======
 namespace Ui {
 class MainWindow;
 }
->>>>>>> 9c0b0f6 (Added quit button)
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
